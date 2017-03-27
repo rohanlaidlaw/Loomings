86f4a9d924d06357d4377ee21ca8d31e627be02a
RLaidlaw <n9433759@qut.edu.au>
2017-03-27 19:42:56 +1000

    Skeleton
    Initialized the screen, implemented player movement .

---
+++ setup.py
@@ -0,0 +1,57 @@
+import libtcodpy as libtcod
+
+
+# INPUT ################################################################################################################
+
+def handle_keys():
+    global player_x, player_y
+
+    key = libtcod.console_wait_for_keypress(True)
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return True
+
+    # Arrow Keys -------------------------------------------------------------------------------------------------------
+    if key.vk == libtcod.KEY_UP:
+        player_y -= 1
+    
+    elif key.vk == libtcod.KEY_DOWN:
+        player_y += 1
+
+    elif key.vk == libtcod.KEY_LEFT:
+        player_x -= 1
+
+    elif key.vk == libtcod.KEY_RIGHT:
+        player_x += 1
+
+
+# GENERAL SET UP #######################################################################################################
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20  # 20 frames-per-second maximum
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'Loomings', False)
+libtcod.sys_set_fps(LIMIT_FPS)
+
+player_x = SCREEN_WIDTH / 2
+player_y = SCREEN_HEIGHT / 2
+
+
+# PRIMARY EVENT LOOP ###################################################################################################
+
+while not libtcod.console_is_window_closed():
+
+    # Draw the Player --------------------------------------------------------------------------------------------------
+    libtcod.console_set_default_foreground(0, libtcod.white)
+    libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+
+    libtcod.console_flush()
+
+    libtcod.console_put_char(0, player_x, player_y, ' ', libtcod.BKGND_NONE)
+
+    exit = handle_keys()
+    if exit:
+        break
