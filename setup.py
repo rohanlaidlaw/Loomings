d5f58225447922a8ce12f77eb956326619336281
nsv <rohanlaidlaw092@gmail.com>
2017-10-26 21:20:05 +1000

    Generic Objects

--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 import libtcodpy as libtcod
 
 
+# CLASSES ##############################################################################################################
+
+class Object:
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        libtcod.console_set_default_foreground(con, self.color)
+        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def clear(self):
+        libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
 # INPUT ################################################################################################################
 
 def handle_keys():
-    global player_x, player_y
-
     key = libtcod.console_wait_for_keypress(True)
 
     if key.vk == libtcod.KEY_ENTER and key.lalt:
@@ -15,30 +33,32 @@ def handle_keys():
         return True
 
     # Arrow Keys -------------------------------------------------------------------------------------------------------
-    if key.vk == libtcod.KEY_UP:
-        player_y -= 1
-    
-    elif key.vk == libtcod.KEY_DOWN:
-        player_y += 1
+    if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+        player.move(0, -1)
 
-    elif key.vk == libtcod.KEY_LEFT:
-        player_x -= 1
+    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+        player.move(0, 1)
 
-    elif key.vk == libtcod.KEY_RIGHT:
-        player_x += 1
+    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+        player.move(-1, 0)
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+        player.move(1, 0)
 
 
 # GENERAL SET UP #######################################################################################################
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
-LIMIT_FPS = 20  # 20 frames-per-second maximum
+LIMIT_FPS = 20
+
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'Loomings', False)
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 libtcod.sys_set_fps(LIMIT_FPS)
 
-player_x = SCREEN_WIDTH / 2
-player_y = SCREEN_HEIGHT / 2
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player]
 
 
 # PRIMARY EVENT LOOP ###################################################################################################
@@ -46,13 +66,14 @@ player_y = SCREEN_HEIGHT / 2
 while not libtcod.console_is_window_closed():
 
     # Draw the Player --------------------------------------------------------------------------------------------------
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+    for object in objects:
+        object.draw()
 
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     libtcod.console_flush()
 
-    libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+    for object in objects:
+        object.clear()
 
     exit = handle_keys()
     if exit:
